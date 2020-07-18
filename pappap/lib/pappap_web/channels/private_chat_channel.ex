121669b7c89bd48dd155a58752d5c0ff547e68b3
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-07-18 16:26:01 +0900

    websocketのメッセージ

---
+++ pappap/lib/pappap_web/channels/private_chat_channel.ex
@@ -0,0 +1,17 @@
+defmodule PappapWeb.PrivateChatChannel do
+  use Phoenix.Channel
+
+  # This function is called when client connects to this server.
+  def join("private_chat:lobby", _msg, socket) do
+    {:ok, socket}
+  end
+
+  def join("private_chat:" <> _private_room_id, _auth_msg, _socket) do
+    {:error, %{reason: "unauthorized"}}
+  end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast!(socket, "new_msg", %{body: body})
+    {:noreply, socket}
+  end
+end
--- pappap/lib/pappap_web/channels/user_socket.ex
+++ pappap/lib/pappap_web/channels/user_socket.ex
@@ -3,6 +3,7 @@ defmodule PappapWeb.UserSocket do
 
   ## Channels
   # channel "room:*", PappapWeb.RoomChannel
+  channel "private_chat:*", PappapWeb.PrivateChatChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
