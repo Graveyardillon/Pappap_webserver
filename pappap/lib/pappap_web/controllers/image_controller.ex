88b91b61a2d1172108377c752bd3d9310abb68a2
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-07-30 22:44:13 +0900

    staticディレクトリの作成

--- pappap/lib/pappap_web/controllers/image_controller.ex
+++ pappap/lib/pappap_web/controllers/image_controller.ex
@@ -3,7 +3,7 @@ defmodule PappapWeb.ImageController do
 
   def upload(conn, %{"image_b64" => image_b64}) do
     "data:image/png;base64," <> raw = image_b64
-    File.write!("./a.png", Base.decode64!(raw))
+    File.write!("./static/image/a.png", Base.decode64!(raw))
 
     json(conn, %{message: "done"})
   end
