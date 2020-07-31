520e507b8034a6f31c6e63b7e000bb46ce071378
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-07-31 18:28:02 +0900

    アップロードの形式の追加

--- pappap/lib/pappap_web/controllers/image_controller.ex
+++ pappap/lib/pappap_web/controllers/image_controller.ex
@@ -2,10 +2,18 @@ defmodule PappapWeb.ImageController do
   use PappapWeb, :controller
 
   def upload(conn, %{"image_b64" => image_b64}) do
-    "data:image/png;base64," <> raw = image_b64
-    uuid = SecureRandom.uuid()
-    File.write!("./static/image/#{uuid}.png", Base.decode64!(raw))
+    if String.starts_with?(image_b64, "data:image/png;base64,") do
+      "data:image/png;base64," <> raw = image_b64
+      uuid = SecureRandom.uuid()
+      File.write!("./static/image/#{uuid}.png", Base.decode64!(raw))
 
-    json(conn, %{local_path: uuid})
+      json(conn, %{local_path: uuid})
+    else
+      raw = image_b64
+      uuid = SecureRandom.uuid()
+      File.write!("./static/image/#{uuid}.png", Base.decode64!(raw))
+
+      json(conn, %{local_path: uuid})
+    end
   end
 end
