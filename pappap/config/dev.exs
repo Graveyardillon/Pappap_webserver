c5829e7e8ace06c84baaafc399b8ee7bcaf2b501
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-08-17 13:31:45 +0900

    assetsの追加

--- pappap/config/dev.exs
+++ pappap/config/dev.exs
@@ -26,7 +26,15 @@ config :pappap, PappapWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [
+    node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 # ## SSL Support
 #
