c0454dbf01b07ee0482b9f28fab1b804cca49250
Papillon6814 <41826375+Papillon6814@users.noreply.github.com>
2020-08-21 23:21:35 +0900

    assets削除

--- pappap/config/dev.exs
+++ pappap/config/dev.exs
@@ -26,15 +26,7 @@ config :pappap, PappapWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
-  ]
+  watchers: []
 
 # ## SSL Support
 #
