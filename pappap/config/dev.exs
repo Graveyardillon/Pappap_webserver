2fe6473afaaacbe6de20d3e6c69cb77adbbe7288
Papillon6814 <so1rou.k@gmail.com>
2020-09-21 21:02:14 +0900

    動作するコンテナの作成（DBサーバーとWebサーバーの連携はコンテナ間でできない）

--- pappap/config/dev.exs
+++ pappap/config/dev.exs
@@ -6,6 +6,7 @@ config :pappap, Pappap.Repo,
   password: "postgres",
   database: "pappap_dev",
   hostname: "localhost",
+  #hostname: "pappap_db",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
