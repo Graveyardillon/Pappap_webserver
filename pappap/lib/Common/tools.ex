c15f2af2aad1068dcdce8adaef1303817356c335
maru713 <31789637+maru713@users.noreply.github.com>
2020-10-05 12:05:13 +0900

    toolのエラー修正,startじにlog書き込み機能実装

--- pappap/lib/Common/tools.ex
+++ pappap/lib/Common/tools.ex
@@ -1,12 +1,17 @@
 defmodule Common.Tools do
 defmacro __using__(_opts) do
     quote do
-      def sendHTTP(url, params) do
+      def sendHTTP(url, params, type) do
         with {:ok, attrs} <- Poison.encode(params),
-          {:ok, response} <- HTTPoison.post(url, attrs, @content_type),
+          {:ok, response} <- HTTPoison.post(url, attrs, type),
           {:ok, body} <- Poison.decode(response.body) do
             body
         else
+          {:error, {reason, _, _}} ->
+            %{
+              "result" => false,
+              "reason" => reason
+            }
           {:error, reason} ->
             %{
               "result" => false,
--- pappap/lib/pappap_web/controllers/tournament_controller.ex
+++ pappap/lib/pappap_web/controllers/tournament_controller.ex
@@ -5,36 +5,49 @@ defmodule PappapWeb.TournamentController do
   @db_domain_url "http://localhost:4000"
   @api_url "/api"
   @tournament_url "/tournament"
+  @tournament_log_url "/tournament_log"
   @get_participating_tournaments_url "/tournament/get_participating_tournaments"
   @get_tournament_topics_url "/tournament/get_tabs"
   @match_start_url "/start"
+  @get_url "/get"
+  @add_url "/add"
   @delete_loser_url "/deleteloser"
   @content_type [{"Content-Type", "application/json"}]
 
   def get_participating_tournaments(conn, params) do
     map =
       @db_domain_url <> @api_url <> @get_participating_tournaments_url
-      |>sendHTTP(params)
+      |>sendHTTP(params, @content_type)
       json(conn,map)
   end
 
   def get_tournament_topics(conn, params) do
     map =
       @db_domain_url <> @api_url <> @get_tournament_topics_url
-      |>sendHTTP(params)
+      |>sendHTTP(params, @content_type)
       json(conn,map)
   end
   def start(conn, params) do
+    log = Task.async(PappapWeb.TournamentController, :add_log, [params])
     map =
       @db_domain_url <> @api_url <> @tournament_url <> @match_start_url
-      |>sendHTTP(params)
-      json(conn, map)
-    end
+      |>sendHTTP(params, @content_type)
+    Task.await(log)
+    json(conn, map)
+  end
+  def add_log(params) do
+    IO.inspect(params)
+    tournament_data =
+      @db_domain_url <> @api_url <> @tournament_url <> @get_url
+      |>sendHTTP(params["tournament"], @content_type)
+      |>IO.inspect(label: :add_log)
+    @db_domain_url <> @api_url <> @tournament_log_url <> @add_url
+    |>sendHTTP(tournament_data, @content_type)
   end
   def delete_loser(conn, params) do
     map =
       @db_domain_url <> @api_url <> @tournament_url <> @delete_loser_url
-      |>sendHTTP(params)
+      |>sendHTTP(params, @content_type)
     json(conn, map)
   end
 end
