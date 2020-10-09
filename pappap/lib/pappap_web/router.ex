2a7c1d225551225cff6a848ef7f449f7769e4e55
maru713 <31789637+maru713@users.noreply.github.com>
2020-10-09 14:39:08 +0900

    chatroomlog記録機能

--- pappap/lib/pappap_web/router.ex
+++ pappap/lib/pappap_web/router.ex
@@ -34,6 +34,8 @@ defmodule PappapWeb.Router do
     post "/tournament/start", TournamentController, :start
     post "/tournament/deleteloser", TournamentController, :delete_loser
 
+    post "/chat/chat_room/create", ChatController, :create_chatroom
+
     post "/profileupdate", ProfileController, :send
     post "/register/device", DeviceController, :register_device_id
 
