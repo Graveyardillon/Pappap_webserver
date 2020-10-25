23a86d148865d4d1db0bb75c7f2338169a35d802
Papillon6814 <so1rou.k@gmail.com>
2020-10-25 13:47:24 +0900

    replace sendHTTP by send_json

--- pappap/lib/pappap_web/controllers/chat_controller.ex
+++ pappap/lib/pappap_web/controllers/chat_controller.ex
@@ -22,7 +22,7 @@ defmodule PappapWeb.ChatController do
   def create_chats(conn, params) do
     map =
       @db_domain_url <> @api_url <> @chats_url
-      |>sendHTTP(params, @content_type)
+      |>send_json(params, @content_type)
     index = 
     Map.get(map, "data")
     |>Map.get("index")
@@ -31,16 +31,16 @@ defmodule PappapWeb.ChatController do
       |>Map.put("index", index)
       |>IO.inspect()
     @db_domain_url <> @api_url <> @chats_log_url
-    |>sendHTTP(merged_map, @content_type)
+    |>send_json(merged_map, @content_type)
     |>IO.inspect
     json(conn, map)
   end
   def create_chatmember(conn, params) do
     map =
       @db_domain_url <> @api_url <> @chat_member_url
-      |>sendHTTP(params, @content_type)
+      |>send_json(params, @content_type)
     @db_domain_url <> @api_url <> @chat_member_log_url
-    |>sendHTTP(map, @content_type)
+    |>send_json(map, @content_type)
     json(conn, map)
   end
 end
