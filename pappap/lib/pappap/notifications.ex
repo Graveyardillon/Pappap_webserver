e1f68a4cb2617307e1d38a8fb244288e8ec8980d
Papillon6814 <so1rou.k@gmail.com>
2020-11-09 02:47:10 +0900

    コメントアウト解除

--- pappap/lib/pappap/notifications.ex
+++ pappap/lib/pappap/notifications.ex
@@ -10,8 +10,8 @@ defmodule Pappap.Notifications do
   def topic, do: "Papillon-inc.eplayers"
 
   def push(message, device_id) do
-    # Pigeon.APNS.Notification.new(message, device_id, Notifications.topic())
-    # |> Pigeon.APNS.push()
+    Pigeon.APNS.Notification.new(message, device_id, Notifications.topic())
+    |> Pigeon.APNS.push()
 
     device = Accounts.from_device_id(device_id)
     params = %{"notif" => %{"user_id" => device.user_id, "content" => message}}
