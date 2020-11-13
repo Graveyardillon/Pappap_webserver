8dbad668f21e23817ed8f0c9f44ba822f0851a12
makikyou <44311434+makikyou@users.noreply.github.com>
2020-11-14 01:01:00 +0900

    onlineなどの通知機能

--- pappap/lib/pappap_web/channels/online_channel.ex
+++ pappap/lib/pappap_web/channels/online_channel.ex
@@ -2,14 +2,15 @@ defmodule PappapWeb.OnlineChannel do
   use PappapWeb, :channel
   alias PappapWeb.Presence
 
-  def join("online", %{"user_id" => user_id}, socket) do
+  def join("online", %{"user_id" => user_id, "chat_room" => chat_room}, socket) do
     # send(self(), {:after_join, user_id})
-    IO.inspect socket
-    {:ok, _} = Presence.track(socket, "online", %{
+    IO.inspect Presence.list(socket)
+    {:ok, _} = Presence.track(socket, "#{inspect socket.transport_pid}", %{
+      chat_room: chat_room,
       user_id: user_id,
       online_at: inspect(System.system_time(:second))
     })
-    Presence.track(socket, socket.channel_pid, %{user_id: user_id})
+    # Presence.track(socket, socket.channel_pid, %{user_id: user_id})
     {:ok, socket}
   end
 
@@ -23,19 +24,60 @@ defmodule PappapWeb.OnlineChannel do
     # {:noreply, socket}
   end
 
-  def handle_in("get_online", params, socket) do
-    IO.inspect socket
-    %{"online"=>%{metas: metas}} = Presence.list(socket) |> IO.inspect
-    list = Enum.map(metas, fn m -> m.user_id end)
+  def handle_in("get_online", _params, socket) do
+    # %{"online"=>%{metas: metas}} = Presence.list(socket)
+    # list = Enum.map(metas, fn m -> m.user_id end)
+    list = Enum.map(Presence.list(socket), fn {_k,v} ->
+      hd(v.metas).user_id
+    end)
     push(socket,"get_online",%{online: list})
     {:noreply, socket}
   end
 
   intercept ["presence_diff"]
-  def handle_out("presence_diff", param, socket) do
-    # IO.inspect param
-    IO.inspect Presence.get_by_key("online",socket.channel_pid)
-    push(socket, "presence_state", Presence.list(socket))
+  def handle_out("presence_diff", %{joins: joins, leaves: leaves}, socket) do
+    chat_room = Presence.get_by_key("online", "#{inspect socket.transport_pid}")
+      |> Map.get(:metas)
+      |> hd
+      |> Map.get(:chat_room)
+
+    if !Enum.empty?(joins) do
+      map = Map.values(joins)
+        |> Enum.map(fn %{metas: metas} ->
+          x = metas
+            |> hd
+          Enum.any?(x.chat_room, fn a -> Enum.member?(chat_room, a) end)
+          |> if do
+            x.user_id
+          else
+            nil
+          end
+        end)
+        |> Enum.filter(fn x -> !is_nil(x) end)
+      if !Enum.empty?(map) do
+        push(socket, "online", %{online: map})
+      end
+    end
+
+    if !Enum.empty?(leaves) do
+      map = Map.values(leaves)
+        |> Enum.map(fn %{metas: metas} ->
+          x = metas
+            |> hd
+          Enum.any?(x.chat_room, fn a -> Enum.member?(chat_room, a) end)
+          |> if do
+            x.user_id
+          else
+            nil
+          end
+        end)
+        |> Enum.filter(fn x -> !is_nil(x) end)
+      if !Enum.empty?(map) do
+        push(socket, "offline", %{offline: map})
+      end
+    end
+
+    # IO.inspect "#{inspect socket.channel_pid}"
     {:noreply, socket}
   end
 end
