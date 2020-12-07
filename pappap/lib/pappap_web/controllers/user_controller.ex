4d8ec346b057458d02826c5ab8cff0fd65b18ee0
Papillon6814 <so1rou.k@gmail.com>
2020-12-07 14:49:31 +0900

    user/getのメソッドをgetに変更

--- pappap/lib/pappap_web/controllers/user_controller.ex
+++ pappap/lib/pappap_web/controllers/user_controller.ex
@@ -7,10 +7,10 @@ defmodule PappapWeb.UserController do
   @get_url "/user/get"
   @get_with_room_id_url "/chat_room/private_room"
 
-  def get(conn, params) do
+  def get(conn, %{"id" => id}) do
     map =
-      @db_domain_url <> @api_url <> @get_url
-      |> send_json(params)
+      @db_domain_url <> @api_url <> @get_url <> "?id=" <> to_string(id)
+      |> get_request()
 
     json(conn, map)
   end
