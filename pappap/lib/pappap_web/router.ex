fec50046e44f2788cc43a350835aafdfc9b8c42f
Papillon6814 <so1rou.k@gmail.com>
2021-07-27 14:08:43 +0900

    feat: optionsの追加

--- pappap/lib/pappap_web/router.ex
+++ pappap/lib/pappap_web/router.ex
@@ -7,11 +7,12 @@ defmodule PappapWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug CORSPlug, origin: ["https://e-players-web.web.app"]
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-    plug CORSPlug
+    plug CORSPlug, origin: ["https://e-players-web.web.app"]
   end
 
   scope "/", PappapWeb do
@@ -35,14 +36,15 @@ defmodule PappapWeb.Router do
     post "/user/get", UserController, :get
     post "/user/get_with_room", UserController, :get_with_room_id
     get  "/user/:string", UserController, :pass_get_request
-    options "/user/:string", UserController, :pass_post_request
     post "/user/:string", UserController, :pass_post_request
     delete "/user/:string", UserController, :pass_delete_request
+    options "/user/:string", UserController, :options
 
     get  "/conf/:string", ConfController, :pass_get_request
     post "/conf/:string", ConfController, :pass_post_request
 
     post "/tournament", TournamentController, :create
+    options "/tournament", TournamentController, :options
     post "/tournament/start", TournamentController, :start
     post "/tournament/start_match", TournamentController, :start_match
     post "/tournament/claim_win", TournamentController, :claim_win
@@ -54,6 +56,7 @@ defmodule PappapWeb.Router do
 
     get  "/tournament/:string", TournamentController, :pass_get_request
     post "/tournament/:string", TournamentController, :pass_post_request
+    options "/tournament/:string", TournamentController, :options
     # get  "/tournament/get_game", TournamentController, :get_game
     # TODO: 画像なのでwebserverでもいろいろしないといけない
     # get  "/tournament/get_thumbnail", TournamentController, :get_thumbnail_image
@@ -62,51 +65,69 @@ defmodule PappapWeb.Router do
 
     get  "/relation/:string", RelationController, :pass_get_request
     post "/relation/:string", RelationController, :pass_post_request
+    options "/relation/:string", RelationController, :options
 
     # TODO: チャットのリクエストのルーティングの、ファイルの中の関数を含めた調整
     get  "/chat/:string", ChatController, :pass_get_request
     post "/chat/:string", ChatController, :pass_post_request
+    options "/chat/:string", ChatController, :options
     delete "/chat", ChatController, :delete
 
     post "/chat/chat_room/create", ChatController, :create_chatroom
+    options "/chat/chat_room/create", ChatController, :options
     post "/chat_room/private_rooms", ChatController, :private_rooms
     post "/chat/chat_member/create", ChatController, :create_chatmember
+    options "/chat/chat_member/:string", ChatController, :options
     post "/chat/chats/create", ChatController, :create_chats
+    options "/chat/chats/:string", ChatController, :_options
 
     get  "/chat_room", ChatRoomController, :show
     get  "/chat_room/:string", ChatRoomController, :pass_get_request
     post "/chat_room/:string", ChatRoomController, :pass_post_request
+    options "/chat_room/:string", ChatController, :options
 
     post "/assistant", AssistantController, :create_assistant
+    options "/assistant", AssistantController, :options
 
     post "/entrant", EntrantController, :create
+    options "/entrant", EntrantController, :options
     get  "/entrant/rank/:tournament_id/:user_id", EntrantController, :show_rank
     get  "/entrant/:string", EntrantController, :pass_get_request
     post "/entrant/:string", EntrantController, :pass_post_request
+    options "/entrant/:string", EntrantController, :options
     delete "/entrant/:string", EntrantController, :pass_delete_request
 
     post "/team", TeamController, :create
+    options "/team", TeamController, :options
     delete "/team", TeamController, :delete
     get "/team", TeamController, :show
     get "/team/:string", TeamController, :pass_get_request
     post "/team/:string", TeamController, :pass_post_request
+    options "/team/:string", TeamController, :options
 
     post "/register/device", DeviceController, :register_device_id
+    options "/register/device", DeviceController, :options
 
     get  "/game/:string", GameController, :pass_get_request
     post "/game/:string", GameController, :pass_post_controller
+    options "/game/:string", GameController, :options
 
     get  "/notification/:string", NotificationController, :pass_get_request
     post "/notification/:string", NotificationController, :pass_post_request
+    options "/notification/:string", NotificationController, :options
     delete "/notification/:string", NotificationController, :pass_delete_request
 
     get  "/profile/:string", ProfileController, :pass_get_request
     post "/profile/:string", ProfileController, :pass_post_request
+    options "/profile/:string", ProfileController, :options
     post "/profile", ProfileController, :send
+    options "/profile", ProfileController, :options
 
     post "/sync", SyncController, :sync
+    options "/sync", SyncController, :options
 
     post "/online/all", OnlineController, :get_online_users
+    options  "/online/all", OnlineController, :options
     get  "/online/entrants", OnlineController, :get_online_entrants
   end
 
