81b0bf0a9352008d2afaf0566a21f84f9f1b1084
Papillon6814 <so1rou.k@gmail.com>
2021-10-16 01:23:16 +0900

    fix: チャンネルの処理整理

--- pappap/lib/pappap_web/channels/chat_channel.ex
+++ pappap/lib/pappap_web/channels/chat_channel.ex
@@ -11,18 +11,11 @@ defmodule PappapWeb.ChatChannel do
     {:ok, socket}
   end
 
-  # FIXME: もしかしたら冗長かもしれない
   def handle_in("new_chat", payload, socket) do
     payload
     |> Map.has_key?("chat")
-    |> (if do
+    |> if do
       with {:ok, _response} <- Chat.send_chat(payload) do
-        message = payload["chat"]["word"]
-        partner_id = payload["chat"]["partner_id"]
-
-        Accounts.get_devices_by_user_id(partner_id)
-        |> notify(message, partner_id)
-
         broadcast!(socket, "new_chat", %{payload: payload})
       else
         {:error, _} -> Logger.error("Error on sending chat")
@@ -33,24 +26,6 @@ defmodule PappapWeb.ChatChannel do
     else
       # Map.has_key?/1 が chatじゃなかった場合（直接のメッセージという保証なし）
       with {:ok, response} <- Chat.send_chat(payload) do
-        message = payload["chat_group"]["word"]
-        members = response["members"]
-
-        members
-        |> is_list()
-        |> if do
-          members
-          |> Enum.each(fn member_id ->
-            member_id
-            |> Accounts.get_devices_by_user_id()
-            |> case do
-              [] -> Notifications.create(member_id, message, 4, member_id)
-              device_list when is_list(device_list)
-                -> notify(device_list, message, member_id)
-            end
-          end)
-        end
-
         broadcast!(socket, "new_chat", %{payload: payload})
       else
         {:error, _} -> Logger.error("Error on sending group chat")
@@ -58,7 +33,7 @@ defmodule PappapWeb.ChatChannel do
       end
 
       {:noreply, socket}
-    end)
+    end
   end
 
   defp notify(device_list, message, user_id) do
