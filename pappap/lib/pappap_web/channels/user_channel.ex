49095dc9845431525ceeb835546b8edd73fe3201
Papillon6814 <so1rou.k@gmail.com>
2021-11-17 20:14:21 +0900

    fix: channel周りの設定

--- pappap/lib/pappap_web/channels/user_channel.ex
+++ pappap/lib/pappap_web/channels/user_channel.ex
@@ -1,16 +1,21 @@
 defmodule PappapWeb.UserChannel do
   use Phoenix.Channel
 
-  def join("user:" <> user_id, %{"token" => token}, socket) do
-    {user_id, _} = Integer.parse(user_id)
-
-    %{id: id} = socket.assigns.user
-
-    if id == user_id and authorized?(token) do
+  def join("user:" <> _user_id, %{"token" => token}, socket) do
+    if authorized?(token) do
       {:ok, socket}
     else
-      {:error, %{reason: "unauthorized or invalid comer"}}
+      {:error, %{reason: "unauthorized or invalid connection"}}
     end
+    # {user_id, _} = Integer.parse(user_id)
+
+    # %{id: id} = socket.assigns.user
+
+    # if id == user_id and authorized?(token) do
+    #   {:ok, socket}
+    # else
+    #   {:error, %{reason: "unauthorized or invalid comer"}}
+    # end
   end
 
   # TODO: ここにトークンによる認証処理を書く。
