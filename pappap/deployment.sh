9242d698e1283818a488c550278ac00095bc7c7d
Papillon6814 <so1rou.k@gmail.com>
2022-02-04 00:26:08 +0900

    feat: アラート周りの設定を自動で行うdeployment.shの作成

---
+++ pappap/deployment.sh
@@ -0,0 +1,3 @@
+gcloud alpha monitoring policies update projects/e-players6814/alertPolicies/13218486532386081660 --no-enabled
+gcloud app deploy app-production.yaml
+gcloud alpha monitoring policies update projects/e-players6814/alertPolicies/13218486532386081660 --enabled
